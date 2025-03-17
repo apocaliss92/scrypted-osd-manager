1e75f61a66c44b04f190952311ce478da5cf7adf
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2025-03-17 23:39:54 +0100

    Fix multiple listeners

--- src/cameraMixin.ts
+++ src/cameraMixin.ts
@@ -477,12 +477,14 @@ export default class OsdManagerMixin extends SettingsMixinDeviceBase<any> implem
             }
 
             if (anyObjectDetection) {
-                this.detectionListener = sdk.systemManager.listenDevice(this.id, ScryptedInterface.ObjectDetector, async (_, __, data) => {
-                    const label = (data as ObjectsDetected)?.detections?.find(det => det.className === 'face')?.label;
-                    if (label) {
-                        this.storageSettings.values.lastFace = label;
-                    }
-                });
+                if (!this.detectionListener) {
+                    this.detectionListener = sdk.systemManager.listenDevice(this.id, ScryptedInterface.ObjectDetector, async (_, __, data) => {
+                        const label = (data as ObjectsDetected)?.detections?.find(det => det.className === 'face')?.label;
+                        if (label) {
+                            this.storageSettings.values.lastFace = label;
+                        }
+                    });
+                }
             } else {
                 this.detectionListener && this.detectionListener.removeListener();
                 this.detectionListener = undefined;
